a10bea3e423bbaa67e44d2135383bc63e68132ae
Ben <benheipp@gmail.com>
2013-02-11 23:20:37 -0500

    Finish sign in

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,13 @@
 include ApplicationHelper
+
+def valid_sigin(user)
+	fill_in "Email",	with: user.email
+	fill_in "Password", with: user.Password
+	click_button "Sign in"
+end
+
+RSPEC::Matchers.define :have_error_message do |message|
+	match do |page|
+		page.should have_selector('div.alert.alert-error'), text: message)
+	end
+end
